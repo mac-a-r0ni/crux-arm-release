96c5d17c2ea8664c5e0b4caf41040cd850b32005
Jay Lanagan <1995739+mac-a-r0ni@users.noreply.github.com>
2024-06-25 01:58:25 -0400

    Create raspberrypi5.mk

---
+++ devices/raspberrypi5.mk
@@ -0,0 +1,3 @@
+CFLAGS = -march=armv8.2-a+crc+crypto -mtune=cortex-a76 -O2 -pipe
+COLLECTIONS = raspberrypi5-arm64 core-arm64 core
+RELEASE_TAR_FILE = crux-arm-$(CRUX_ARM_VERSION)-aarch64-raspberrypi5.rootfs.tar.xz
